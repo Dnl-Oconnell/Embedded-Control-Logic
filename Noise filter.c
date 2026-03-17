b8a82031c599c4916d2e847480916e4177093746
Dnl-Oconnell <Doc24478@gmail.com>
2026-03-17 01:07:03 +0000

    Update Noise filter.c
    Added comments to describe thought process while coding

--- Noise filter.c
+++ Noise filter.c
@@ -1,26 +1,51 @@
+/*
+Bridge Sensor Data Filter & Anomaly Detector
+-----------------------------------------------
+Simulates noisy sensor readings from a bridge strain gauge,
+applies a moving average filter, and detects anomalies.
+ 
+Functions:
+  generateSensorData() - generates noisy sine wave signal
+  movingAverage()      - smooths signal using moving average
+  detectAnomalies()    - flags readings above threshold
+  printReport()        - prints formatted results table
+ 
+Usage: run and follow input prompts for array size,
+    noise level, periods, window size and threshold
+ */
+
+
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
 #include <stdlib.h>
 
-
+// Generating a noisy signal
 void generateSensorData(double arr[], int len, int periods, double noise){
     for(int i = 0; i < len; i++){
+
+        // randomising an offset to add to the sin wave
         double n = ((double)rand() / RAND_MAX) * (2 * noise) - noise;
+        // to not overflow the array, the amount of periods will be contained within the bounds of 
+        // the length of the array, multiplied by 2 PI
         double x = (i * periods * 2 * M_PI) / len;
         double y = sin(x) + n;
         arr[i] = y;
     }
 }
 
+// Using a "window" of values to find the average of a number of values to smooth out the noise 
 void movingAverage(double input[], double output[], int len, int window){
     for(int i = 0; i < len; i++){
         double sum = 0;
         int count = 0;
 
+        // need to account for any numbers the program may want to count outside of the bounds of
+        // the array
         for(int j = -window; j <= window; j++){
             if((i + j) >= 0 && (i + j) < len){
                 sum += input[i + j];
+                // keeps track of the number of values added together for average calculation
                 count ++;
             }
         }
@@ -28,10 +53,13 @@ void movingAverage(double input[], double output[], int len, int window){
     }
 }
 
+// finds if any of the values in the smoothed signal are above a certain threshold. If so, their position
+// fill be stored and the amount of anomalies will be returned
 int detectAnomalies(double arr[], int len, double threshold, int anomalies[]){
     int j = 0;
 
     for(int i = 0; i < len; i ++){
+        // if value in arr[] is larger than the threshold, store the location (value of i)
         if(arr[i] > threshold){
             anomalies[j] = i;
             j++;
@@ -40,11 +68,13 @@ int detectAnomalies(double arr[], int len, double threshold, int anomalies[]){
     return j;
 }
 
+// printing the results in a neat table with an alert in case the values exceeded the threshold
 void printReport(double noisySignal[], double smoothSignal[], int len, int anomalies[], int numAnomalies){
     printf("Index %-6| Noisy %-18| Smooth %-18| Status|\n");
     printf("------------------------------\n");
     int j = 0;
     for(int i = 0; i < len; i ++){
+        // checks if the current value of i is also a value above the error from detectAnomalies
         if(j < numAnomalies && i == anomalies[j]){
             printf("%-6d| %-10f| %-10f| ALERT\n", i, noisySignal[i], smoothSignal[i]);
             j ++;
@@ -57,10 +87,10 @@ void printReport(double noisySignal[], double smoothSignal[], int len, int anoma
 
 
 int main(){
-
-
+    // seeding randomiser with time
     srand(time(NULL));
 
+    // asks user for each variable input
     int sizeOfArray;
     printf("Enter array size: ");
     scanf("%d", &sizeOfArray);
